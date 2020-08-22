42fd86bd9627da5268d1f8aa017da865af1db26a
yuanzhibx <ybx0729@163.com>
2020-08-22 11:29:26 +0800

    Yuanzhibx 【栈】 中缀表达式转换为后缀表达式

--- data_structures/src/com/yanbingxu/stack/PolandNotation.java
+++ data_structures/src/com/yanbingxu/stack/PolandNotation.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 逆波兰计算器
+ * 逆波兰计算器 & 中缀表达式转换为后缀表达式
  *
  * @author Yuanzhibx
  * @Date 2020-08-20
@@ -24,6 +24,25 @@ public class PolandNotation {
         System.out.println("list = " + list);
         int res = calculate(list);
         System.out.println("计算结果为 = " + res);
+
+        System.out.println("-----------------------");
+
+        /*
+            完成将一个中缀表达式转成后缀表达式的功能
+            1+((2+3)×4)-5  -->  1 2 3 + 4 × + 5 –
+            说明:
+            1. 因为直接对 str 进行操作不方便，因此 先将 "1+((2+3)×4)-5" 中缀表达式 --> 对应的 List
+                即 "1+((2+3)×4)-5" --> ArrayList [1, +, (, (, 2, +, 3, ), *, 4, ), -, 5]
+            2. 将得到的中缀表达式对应的 List --> 后缀表达式对应的 List
+                即 ArrayList [1, +, (, (, 2, +, 3, ), *, 4, ), -, 5] --> ArrayList [1, 2, 3, +, 4, *, +, 5, –]
+         */
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println("后缀表达式对应的 List" + infixExpressionList);
+        List<String> parseSuffixExpresionList = parseSuffixExpresionList(infixExpressionList);
+        System.out.println("后缀表达式对应的 List" + parseSuffixExpresionList);
+
+        System.out.printf("expression = %d", calculate(parseSuffixExpresionList));
     }
 
     /**
@@ -78,10 +97,139 @@ public class PolandNotation {
                     throw new RuntimeException("运算符有误~~~");
                 }
                 // 把 res 入栈
-                stack.push("" +res);
+                stack.push("" + res);
             }
         }
         return Integer.parseInt(stack.pop());
+    }
+
+    /**
+     * 将中缀表达式转为对应的 List
+     *
+     * @param s 中缀表达式 --> 1+((2+3)×4)-5
+     * @return
+     */
+    public static List<String> toInfixExpressionList(String s) {
+        // 存放中缀表达式对应的内容
+        List<String> list = new ArrayList<>();
+        // 指针, 用于遍历中缀表达式字符串
+        int i = 0;
+        // 用于对多位数的拼接
+        String str;
+        // 遍历一个字符, 就放入 c 中
+        char c;
+        do {
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                // c 为非数字, 则加入 list 中
+                list.add("" + c);
+                i++;
+            } else {
+                // 如果为数, 则考虑多位数情况
+                str = "";
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                    // 拼接
+                    str += c;
+                    i++;
+                }
+                list.add(str);
+            }
+        } while (i < s.length());
+        return list;
+    }
+
+    /**
+     * 将中缀表达式对应的 List 转为后缀表达式对应的 List
+     */
+    public static List<String> parseSuffixExpresionList(List<String> ls) {
+        // 运算符栈
+        Stack<String> s1 = new Stack<>();
+        // 使用 ArrayList 代替存储中间节点栈, 方便操作
+//        Stack<String> s2 = new Stack<>();
+        List<String> s2 = new ArrayList<>();
+
+        // 遍历 ls
+        for (String item : ls) {
+            if (item.matches("\\d+")) {
+                // 如果为数, 则加入 s2
+                s2.add(item);
+            } else if (item.equals("(")) {
+                s1.push(item);
+            } else if (item.equals(")")) {
+                // 如果是右括号 ")"，则依次弹出 s1 栈顶的运算符，并压入 s2，直到遇到左括号为止，此时将这一对括号丢弃
+                while (!s1.peek().equals("(")) {
+                    s2.add(s1.pop());
+                }
+                // 将 ( 弹出 s1 栈, 消除小括号
+                s1.pop();
+            } else {
+                // item 的优先级 小于等于 s1 栈顶运算符, 将 s1 栈顶的运算符弹出并加入到 s2, 再次转到(4.1)与 s1 中新的栈顶运算符相比较
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)) {
+                    // 将 s1 栈顶的运算符弹出并加入到 s2
+                    s2.add(s1.pop());
+                }
+                // 将 item 压入 s1 栈中
+                s1.push(item);
+            }
+        }
+
+        // 将 s1 中剩余的运算符异常弹出并加入到 s2
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+        // 因为存放到 list 中, 所以按顺序输出就是对应的后缀表达式 List
+        return s2;
+    }
+
+}
+
+/**
+ * 可返回 运算符 对应的优先级
+ */
+class Operation {
+
+    /**
+     * 加
+     */
+    private static int ADD = 1;
+
+    /**
+     * 减
+     */
+    private static int SUB = 1;
+
+    /**
+     * 乘
+     */
+    private static int MUL = 2;
+
+    /**
+     * 除
+     */
+    private static int DIV = 2;
+
+    /**
+     * 返回对应的优先级数字
+     */
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该预算符");
+                break;
+        }
+        return result;
     }
 
 }
